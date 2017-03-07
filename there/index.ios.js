51d88cf37d0c6a15305cccb72ee7d8b618a95dda
lmn <lmnhot@hotmail.com>
2017-03-07 23:49:49 +0800

    init

---
+++ there/index.ios.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Home from './src/pages/Home'
+
+export default class there extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+	      <Home></Home>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('there', () => there);
